df77e010839908ac1c2cfd96bba213aae4c87924
tracy <200548417@student.georgianc.on.ca>
2024-10-15 11:43:10 -0400

    Add the function for Login and SignUp

--- app/src/main/java/com/example/assignment1_login/Login.kt
+++ app/src/main/java/com/example/assignment1_login/Login.kt
@@ -1,16 +1,56 @@
 package com.example.assignment1_login
 
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.assignment1_login.databinding.ActivityLoginBinding
+import com.example.assignment1_login.databinding.ActivityMainBinding
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener{
+            val enteredUsername = binding.etUsername.text.toString()
+            val enteredPassword = binding.etPassword.text.toString()
+
+            val sharedPreferences: SharedPreferences = getSharedPreferences("User", MODE_PRIVATE)
+            val storedUsername = sharedPreferences.getString("USERNAME_$enteredUsername", null)
+            val storedPassword = sharedPreferences.getString("PASSWORD_$enteredPassword", null)
+
+            if(storedUsername == null){
+                Toast.makeText(this, "Username does not exit.", Toast.LENGTH_SHORT).show()
+            }else{
+                if(storedPassword == enteredPassword){
+                    Toast.makeText(this, "Log in successful.", Toast.LENGTH_SHORT).show()
+                    val gitHubUrl = "https://pages.github.com/"
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(gitHubUrl))
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(this, "Incorrect password", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        binding.btnReturn.setOnClickListener{
+            val intent = Intent(this, ActivityMainBinding :: class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/assignment1_login/SignUp.kt
+++ app/src/main/java/com/example/assignment1_login/SignUp.kt
@@ -1,16 +1,58 @@
 package com.example.assignment1_login
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.assignment1_login.databinding.ActivityMainBinding
+import com.example.assignment1_login.databinding.ActivitySignUpBinding
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnSignup.setOnClickListener{
+            val username = binding.etUsername.text.toString()
+            val password = binding.etPassword.text.toString()
+            val confirmPassword = binding.etConfirmPassword.text.toString()
+
+            if(password != confirmPassword){
+                Toast.makeText(this, "Password do not match", Toast.LENGTH_SHORT).show()
+            }else{
+                val sharedPreferences: SharedPreferences = getSharedPreferences("User", MODE_PRIVATE)
+                val editor: SharedPreferences.Editor = sharedPreferences.edit()
+
+                val inputUsername = sharedPreferences.getString("USERNAME_$username", null)
+
+                if(inputUsername != null){
+                    Toast.makeText(this, "Username already exists. Choose another username.", Toast.LENGTH_SHORT).show()
+                }else{
+                    editor.putString("USERNAME_$username", username)
+                    editor.putString("PASSWORD_$password", password)
+                    editor.apply()
+
+                    Toast.makeText(this, "Account Created!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        binding.btnReturn.setOnClickListener{
+            val intent = Intent(this, ActivityMainBinding :: class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
