cdbe1d12879088d9f5b71c2cc1bc04915a0b4788
tracy <200548417@student.georgianc.on.ca>
2024-10-16 23:41:35 -0400

    Add check if email is valid function to SignUp2 page.

--- app/src/main/java/com/example/assignment1_login/SignUp2.kt
+++ app/src/main/java/com/example/assignment1_login/SignUp2.kt
@@ -1,16 +1,55 @@
 package com.example.assignment1_login
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.assignment1_login.databinding.ActivitySignUp2Binding
+import java.util.regex.Pattern
 
 class SignUp2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUp2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up2)
+
+        binding = ActivitySignUp2Binding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnSignup.setOnClickListener{
+            val username = binding.etUsername.text.toString()
+            val password = binding.etPassword.text.toString()
+            val confirmPassword = binding.etConfirmPassword.text.toString()
+
+            if(!Patterns.EMAIL_ADDRESS.matcher(username).matches()) {
+                Toast.makeText(this, "Please enter a valid email address", Toast.LENGTH_SHORT).show()
+            }else if(password != confirmPassword){
+                Toast.makeText(this, "Password do not match", Toast.LENGTH_SHORT).show()
+            }else{
+                val sharedPreferences: SharedPreferences = getSharedPreferences("User", MODE_PRIVATE)
+                val editor: SharedPreferences.Editor = sharedPreferences.edit()
+
+                val inputUsername = sharedPreferences.getString("USERNAME_$username", null)
+
+                if(inputUsername != null){
+                    Toast.makeText(this, "Username already exists. Choose another username.", Toast.LENGTH_SHORT).show()
+                }else{
+                    editor.putString("USERNAME_$username", username)
+                    editor.putString("PASSWORD_$password", password)
+                    editor.apply()
+
+                    Toast.makeText(this, "Account Created!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
